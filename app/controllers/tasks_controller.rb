300007f0d1d0c3ac0ab206da795cc3c68850e2f1
akira-yp <yog.puripuri@gmail.com>
2021-03-13 01:16:40 +0900

    新規task作成機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,26 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice:"新しいタスクを追加しました"
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
