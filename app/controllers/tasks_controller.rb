5803218bc906701d2ac105631f0b7b683c2a6332
akira-yp <yog.puripuri@gmail.com>
2021-03-13 01:49:19 +0900

    タスク編集機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :create, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,13 +14,28 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice:"新しいタスクを追加しました"
+      redirect_to tasks_path, notice: "新しいタスクを追加しました"
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスク内容を変更しました"
+    else
+      render :edit
+    end
+  end
+
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:title, :content)
   end
