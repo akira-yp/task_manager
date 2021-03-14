c6388da61e58d68a4d9dd7111d1d1586de425db5
akira-yp <yog.puripuri@gmail.com>
2021-03-15 00:03:11 +0900

    indexで新しい投稿順に表示変更と、テストコード作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :task do
-    title { 'test_title' }
-    content { 'test_content' }
+    title { 'test_title1' }
+    content { 'test_content1' }
+  end
+
+  factory :second_task,class: Task do
+    title { 'test_title2' }
+    content { 'test_content2' }
+  end
+
+  factory :third_task, class: Task do
+    title { 'test_title3' }
+    content { 'test_content3' }
   end
 end
