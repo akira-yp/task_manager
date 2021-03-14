e0cb4f056ef69a86c1576eac48b78f161b25b474
akira-yp <yog.puripuri@gmail.com>
2021-03-14 18:23:56 +0900

    表示の国際化とタイムゾーンを 東京に設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TaskManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Asia/Tokyo'
+
     config.generators do |g|
       g.text_framwork :rspec,
         model_specs: true,
