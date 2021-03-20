2a3fdd3511b6aa9e326d349be9b97b9456b4c0dd
akira-yp <yog.puripuri@gmail.com>
2021-03-21 00:37:28 +0900

    ソート機能とそれに付随するテスト項目を追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
+require 'date'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
+        test_day = DateTime.now
         fill_in 'タスク名', with: 'testA'
         fill_in '内容', with: 'test_contentA'
+        fill_in '終了期限', with: test_day
         click_button '登録する'
         expect(page).to have_content 'test_contentA'
       end
@@ -25,10 +28,19 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        visit tasks_path
         task_list = all('.task_row')
         latest_task = task_list[0]
         expect(latest_task).to have_content "test_title3"
+      end
+    end
+    context '終了期限でソートした場合' do
+      before do
+        click_on '終了期限でソート'
+      end
+      it '終了期限が最も新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        oldest_deadline = task_list[0]
+        expect(oldest_deadline).to have_content "test_title2"
       end
     end
   end
