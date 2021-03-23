49eaf9abf5ad08b32aac2915cd69b3f95594323b
akira-yp <yog.puripuri@gmail.com>
2021-03-23 23:12:59 +0900

    titleのindexカラムを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_062126) do
+ActiveRecord::Schema.define(version: 2021_03_23_140644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_062126) do
     t.datetime "expired_at", null: false
     t.integer "status", default: 1, null: false
     t.integer "priority", default: 2
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
