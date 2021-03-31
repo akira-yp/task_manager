6bc87c19df82d20a3e120da9546e376e9fcf6bf0
akira-yp <yog.puripuri@gmail.com>
2021-03-31 22:06:34 +0900

    アソシエーションに伴いタスク機能のシステムスペックを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 require 'date'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:f_user){FactoryBot.create(:first_user)}
-  let!(:task_a){FactoryBot.create(:first_task)}
-  let!(:task_b){FactoryBot.create(:second_task)}
-  let!(:task_c){FactoryBot.create(:third_task)}
+  let!(:task_a){FactoryBot.create(:first_task, user_id: f_user.id)}
+  let!(:task_b){FactoryBot.create(:second_task, user_id: f_user.id)}
+  let!(:task_c){FactoryBot.create(:third_task, user_id: f_user.id)}
+
+  before do
+    visit new_session_path
+    fill_in 'e-mail', with:'first_user@test.com'
+    fill_in 'パスワード', with:'password'
+    click_button 'ログイン'
+  end
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -41,13 +48,11 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
     context '終了期限でソートした場合' do
-      before do
+      it '終了期限が最も古いタスクが一番上に表示される' do
         click_on '終了期限'
-      end
-      it '終了期限が最も新しいタスクが一番上に表示される' do
         task_list = all('.task_row')
         oldest_deadline = task_list[0]
-        expect(oldest_deadline).to have_content "test_title2"
+        expect(oldest_deadline).to have_content "test_title1"
       end
     end
     context '優先順位でソートした場合' do
@@ -60,12 +65,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '検索機能' do
-    let!(:task_a){FactoryBot.create(:first_task)}
-    let!(:task_b){FactoryBot.create(:second_task)}
-    let!(:task_c){FactoryBot.create(:third_task)}
-    before do
-      visit tasks_path
-    end
+    # let!(:task_a){FactoryBot.create(:first_task)}
+    # let!(:task_b){FactoryBot.create(:second_task)}
+    # let!(:task_c){FactoryBot.create(:third_task)}
+    # before do
+    #   visit tasks_path
+    # end
     context 'タイトルであいまい検索をした場合' do
       it '検索キーワードを含むタスクが絞り込まれる' do
         fill_in 'タスク名', with:'title1'
@@ -98,8 +103,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         show_task = FactoryBot.create(:first_task)
-         visit task_path(show_task.id)
+         visit task_path(task_a.id)
          expect(page).to have_content 'test_title1'
        end
      end
