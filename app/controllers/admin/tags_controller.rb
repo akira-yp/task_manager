0a29dd8a68f2a1a54e201559efa1c1910d8337a2
akira-yp <yog.puripuri@gmail.com>
2021-04-04 14:58:33 +0900

    タグ新規作成&一覧機能

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,21 @@
+class Admin::TagsController < ApplicationController
+
+  def new
+    @tag = Tag.new
+    @tags = Tag.all
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to new_admin_tag_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
