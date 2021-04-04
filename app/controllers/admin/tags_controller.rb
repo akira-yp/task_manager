ed518ff74c719e15c21ea53dbcf3c3f6fe2d8797
akira-yp <yog.puripuri@gmail.com>
2021-04-04 15:18:31 +0900

    管理者がタグを削除する機能を作成

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -14,6 +14,15 @@ class Admin::TagsController < ApplicationController
     end
   end
 
+  def destroy
+    @tag = Tag.find(params[:id])
+    if @tag.destroy
+      redirect_to new_admin_tag_path, notice:"#{@tag.name}タグを削除しました"
+    else
+      render :new
+    end
+  end
+
   private
   def tag_params
     params.require(:tag).permit(:name)
